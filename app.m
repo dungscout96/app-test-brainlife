cb1d642db06a1882f94a13bff94833305d7bf8fc
Dung Truong <dt.young112@gmail.com>
2021-08-06 22:59:44 +0000

    switch back to using jq as json library in octave is complicated

--- app.m
+++ app.m
@@ -1,17 +1,8 @@
 addpath('/home/octave/eeglab');
-addpath('/home/octave/JSONio');
 eeglab nogui;
 
-% arg_list = argv();
-% EEG = pop_loadset(arg_list{1});
-% parse config.json
-file = 'config.json';
-fid = fopen(file); 
-raw = fread(fid,inf); 
-str = char(raw');
-args = jsonread(str);
-EEG = pop_loadset(args.set);
-
+arg_list = argv();
+EEG = pop_loadset(arg_list{1});
 %addpath(pwd, 'iclabel');
 %addpath(pwd, 'firfilt');
 EEG = pop_runica(EEG, 'icatype', 'runica');
