692f06d8cf12a03ccdee1b18c48ff5a1dc7ba254
Dung Truong <dt.young112@gmail.com>
2021-08-06 14:30:02 -0700

    let mat script handle parsing config.json

--- app.m
+++ app.m
@@ -1,17 +1,17 @@
-#!/usr/local/bin/octave -qf
-dbstop if error
-dbstop if warning
-disp('here1');
 addpath('/home/octave/eeglab');
-disp('here2');
 eeglab nogui;
-disp('here3');
-arg_list = argv();
-disp('here4');
-EEG = pop_loadset(arg_list{1});
-disp('here5');
+
+% arg_list = argv();
+% EEG = pop_loadset(arg_list{1});
+% parse config.json
+file = 'config.json';
+fid = fopen(file); 
+raw = fread(fid,inf); 
+str = char(raw');
+args = jsondecode(str);
+EEG = pop_loadset(args.set);
+
 %addpath(pwd, 'iclabel');
 %addpath(pwd, 'firfilt');
 EEG = pop_runica(EEG, 'icatype', 'runica');
-disp('here6');
 disp('Success');
